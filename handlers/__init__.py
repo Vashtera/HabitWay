b41e512113d6e07271ecd85b5fe1b684864d8e05
vashtera <raul.aytbaev@gmail.com>
2026-06-11 14:36:40 +0500

    first test

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from aiogram import Dispatcher
+from handlers.profile import router as profile_router
 
 def register_routes(dp: Dispatcher):
-    ...
+    dp.include_router(profile_router)
--- handlers/profile.py
+++ handlers/profile.py
@@ -12,11 +12,11 @@ async def show_profile(message: Message, pool):
     async with pool.acquire() as conn: #взяли соединение
         user = await get_user_by_tg_id(message.from_user.id, conn) #передача в requests.py
     await message.answer(
-        f"Привет! {message.from_user_fullname}"\n
-        f"Дата начала: {user['start_date']}"\n 
-        f"Дней без курения: ..."\n
-        f"Сэкономлено денег: {user['total_save_money']}"\n 
-        f"Не выкурено сигарет: ..."\n
+        f"Привет! {message.from_user_fullname}\n"
+        f"Дата начала: {user['start_date']}\n" 
+        f"Дней без курения: ...\n"
+        f"Сэкономлено денег: {user['total_save_money']}\n" 
+        f"Не выкурено сигарет: ...\n"
 
     )
     
