1dc04bb8e65a61108ec31a92a1e7af08a916e3b8
vashtera <raul.aytbaev@gmail.com>
2026-06-14 22:44:04 +0500

    added checking a new user

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from handlers import register_routes
 from database.initialization import create_pool
 from keyboards.exist_keyboard import keyboard_for_existing_user 
 from middlewares.middleware import DataBaseMiddleware
+from database.requests import get_user_by_tg_id
+from keyboards.unexist_keyboard import keyboard_for_unexist_user
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
@@ -13,10 +15,14 @@ from aiogram.types import Message
 dp = Dispatcher()
 
 
-# Command handler
+# start Command handler
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Привет, я твой трекер по не курению", 
+    if get_user_by_tg_id is None:
+        await message.answer("Привет, давай зарегистрируемся!",
+                             reply_markup=keyboard_for_unexist_user)
+    else:    
+        await message.answer(f"Привет! {message.from_user.full_name} я твой трекер по не курению", 
                          reply_markup=keyboard_for_existing_user)
 
 
