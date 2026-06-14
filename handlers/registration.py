2a2e6cf3dd7bc6cf2454e3e9fca459cb945472bb
vashtera <raul.aytbaev@gmail.com>
2026-06-14 22:31:49 +0500

    pre-final work with registration

--- handlers/registration.py
+++ handlers/registration.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
@@ -16,39 +18,59 @@ class Register(StatesGroup):
 @router.message(F.text == 'Зарегистрироваться')
 async def registration(message: Message, state: FSMContext):
     await message.answer('')
-    await state.set_state(Register.start_date)
-    try:
-        await message.answer("Введите дату, когда Вы бросили курить в формате ДД.ММ.ГГГГ")
-    except ValueError:
-        await message.answer("Пожалуйста введите нужный формат даты!")
+    await state.set_state(Register.cig_price)
+    await message.answer("Введите цену за пачку сигарет")
 
-@router.message(Register.start_date)
-async def reg_start_date(message: Message, state: FSMContext):
-    await state.update_data(start_date=message.text)
-    await state.set_state(Register.cig_in_pack)
+
+@router.message(Register.cig_price)
+async def reg_cig_price(message: Message, state: FSMContext):
     try:
-        await message.answer("Введите количество сигарет в пачке")
+        val_price = float(message.text)
     except ValueError:
-        await message.answer("Пожалуйста введите целое число")
+        await message.answer("Пожалуйста введите число")
+        return
+    await state.update_data(cig_price=val_price)
+    await state.set_state(Register.cig_in_pack)
+    await message.answer("Введите количество сигарет в пачке")
+    
     
 @router.message(Register.cig_in_pack)
 async def reg_cig_in_pack(message: Message, state: FSMContext):
-    await state.update_data(cig_in_pack=message.text)
-    await state.set_state(Register.cig_per_day)
     try:
-        await message.answer("Введите количество сигарет которые Вы потребляете в день")
+        val_cig_in_pack = int(message.text)
     except ValueError:
         await message.answer("Пожалуйста введите целое число")
+        return
+    await state.update_data(cig_in_pack=val_cig_in_pack)
+    await state.set_state(Register.cig_per_day)
+    await message.answer("Введите количество сигарет которые Вы потребляете в день")
+    
 
 @router.message(Register.cig_per_day)
 async def reg_cig_per_day(message: Message, state: FSMContext):
-    await state.update_data(cig_per_day=message.text)
-    await state.set_state(Register.cig_price)
     try:
-        await message.answer("Введите цену за пачку сигарет")
+        val_cig_per_day = int(message.text)
     except ValueError:
-        await message.answer("Пожалуйста введите число")
+        await message.answer("Пожалуйста введите целое число")
+        return
+    await state.update_data(cig_per_day=val_cig_per_day)
+    await state.set_state(Register.start_date)
+    try:
+        await message.answer("Введите дату когда Вы бросили курить в формате ДД.ММ.ГГГГ")
+    except ValueError:
+        await message.answer("Пожалуйста введите нужный формат ДД.ММ.ГГГГ")
     
-@router.message(Register.cig_price)
-async def reg_cig_price(message: Message, state: FSMContext):
+@router.message(Register.start_date)
+async def reg_start_date(message: Message, state: FSMContext):
+    try:
+        val_start_date = datetime.strptime(message.text, '%d.%m.%Y').date()
+    except ValueError:
+        await message.answer("Пожалуйста введите нужный формат ДД.ММ.ГГГГ")
+        return
     data = await state.get_data()
+    tg_id = message.from_user.id
+    fullname = message.from_user.full_name
+    start_date = val_start_date
+    user_cig_in_pack = int(data.get("cig_in_pack"))
+    user_cig_per_day = int(data.get("cig_per_day"))
+    user_cig_price = float(data.get("cig_price"))
