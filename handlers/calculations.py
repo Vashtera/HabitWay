04cfc5c754f09c8c54dbee683c9bfddbdc24c6f9
vashtera <raul.aytbaev@gmail.com>
2026-06-15 14:14:48 +0500

    wrote total not smoked cig func un calculations

--- handlers/calculations.py
+++ handlers/calculations.py
@@ -4,15 +4,21 @@ class Calculate:
     def __init__(
             self,
             cig_in_pack: int, 
-            cig_per_day: int, 
             cig_price: float
     ) -> None:
         self.cig_in_pack = cig_in_pack
-        self.cig_per_day = cig_per_day
         self.cig_price = cig_price
 
-    async def days_without_smoke(self, start_date: str) -> str:
+    def days_without_smoke(self, start_date: str) -> int:
         self.start_date = start_date
         start_date_obj = datetime.strptime(start_date, '%d.%m.%Y').date()
         today = datetime.now().date()
-        days_without = (today - start_date_obj).days()
+        self.days_without_smoking = (today - start_date_obj).days
+        return self.days_without_smoking
+    
+    def total_not_smoked_cigarettes(self, cig_per_day: int) -> int:
+        self.cig_per_day = cig_per_day
+        total = self.days_without_smoking * cig_per_day
+        return total
+
+    
