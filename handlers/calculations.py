fc8c198296ecc814d4c3ee2e0431e5b229d6489b
vashtera <raul.aytbaev@gmail.com>
2026-06-15 14:05:48 +0500

    wrote days without smoke func

--- handlers/calculations.py
+++ handlers/calculations.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+
 class Calculate:
     def __init__(
-            self, 
-            start_date: str, 
+            self,
             cig_in_pack: int, 
             cig_per_day: int, 
             cig_price: float
     ) -> None:
-        self.start_date = start_date
         self.cig_in_pack = cig_in_pack
         self.cig_per_day = cig_per_day
         self.cig_price = cig_price
 
+    async def days_without_smoke(self, start_date: str) -> str:
+        self.start_date = start_date
+        start_date_obj = datetime.strptime(start_date, '%d.%m.%Y').date()
+        today = datetime.now().date()
+        days_without = (today - start_date_obj).days()
