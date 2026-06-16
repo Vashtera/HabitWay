e4b5954ab0de398377997184049505a31ebafecb
vashtera <raul.aytbaev@gmail.com>
2026-06-16 16:49:39 +0500

    edited calculations

--- database/requests.py
+++ database/requests.py
@@ -1,10 +1,10 @@
 from typing import Any
-
+#найти пользователя по айди телеграма
 async def get_user_by_tg_id(tg_id: int, conn):
     return await conn.fetchrow(
         "SELECT * FROM users WHERE tg_id = $1", tg_id
     )
-
+#добавить нового пользователя
 async def add_user(
         tg_id: int,
         name: str,
@@ -15,7 +15,7 @@ async def add_user(
     return await conn.fetchrow(
         "INSERT INTO users (tg_id, fullname, cigarette_id, start_date) VALUES ($1, $2, $3, $4)", tg_id, name, cigarette_id, date
     )
-
+#добавить данные о сигаретах
 async def add_cigarettes(
         cig_in_pack: int,
         cig_per_day: int,
@@ -26,9 +26,14 @@ async def add_cigarettes(
         "INSERT INTO cigarettes (cigarettes_in_pack, cigarettes_per_day, cigarette_price) VALUES ($1, $2, $3) RETURNING cigarette_id", 
         cig_in_pack, cig_per_day, cig_price
     )
-
+#получить данные из обеих таблиц по айди сигарет
 async def get_data_from_all_tables(tg_id: int, conn):
     return await conn.fetchrow(
         "SELECT * FROM users JOIN cigarettes ON users.cigarette_id = cigarettes.cigarette_id WHERE tg_id = $1", 
         tg_id
+    )
+
+async def add_money(total_saved_money: float, conn):
+    return await conn.fetchrow(
+        "INSERT INTO users (total_save_money) VALUES ($1)", total_saved_money
     )
--- handlers/calculations.py
+++ handlers/calculations.py
@@ -1,27 +1,28 @@
 from datetime import datetime
 
+from database.requests import add_money
+
 class Calculate:
+    def __init__(self):
+        self.days_without_smoking = 0
+        self.total_cig = 0
+        self.price = 0.0
        
     def days_without_smoke(self, start_date: str) -> int:
         start_date_obj = datetime.strptime(start_date, '%Y-%m-%d').date()
         today = datetime.now().date()
-        global days_without_smoking
-        days_without_smoking = (today - start_date_obj).days
-        return days_without_smoking
+        self.days_without_smoking = (today - start_date_obj).days
+        return self.days_without_smoking
     
     def total_not_smoked_cigarettes(self, cig_per_day: int) -> int:
-        global total_cig
-        total_cig = days_without_smoking * cig_per_day
-        return total_cig
+        self.total_cig = self.days_without_smoking * cig_per_day
+        return self.total_cig
 
     def one_cig_price(self, cig_in_pack: int, cig_price: float) -> float:
-        global price
-        price = cig_price / cig_in_pack
-        return price
+        self.price = cig_price / cig_in_pack
+        return self.price
     
     async def total_saved_money(self, conn) -> float:
-        total = total_cig * price
-        await conn.fetchrow(
-        "INSERT INTO users (total_save_money) VALUES ($1)", total
-        )
-        return round(total, 2)
+        total = self.total_cig * self.price
+        request = await add_money(total, conn)
+        return request
