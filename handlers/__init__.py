8a3034113448e899e91efc2881595564932e8f09
vashtera <raul.aytbaev@gmail.com>
2026-06-17 14:27:21 +0500

    included reset handler

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Dispatcher
 from handlers.profile import router as profile_router
 from handlers.registration import router as registration_router
 from handlers.add_changes import router as changes_router
+from handlers.reset import router as reset_router
 
 def register_routes(dp: Dispatcher):
     dp.include_router(profile_router)
     dp.include_router(registration_router)
     dp.include_router(changes_router)
+    dp.include_router(reset_router)
